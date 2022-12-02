a537abd70fbfdcf424acd4946099e64f4faa2ba2
volodymyrboichuk <volodymyr.boichuk@logivations.com>
2022-12-02 18:10:14 +0200

    webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.join(__dirname, "/dist"), // the bundle output path
+        filename: "bundle.js", // the name of the bundle
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html", // to import index.html file inside index.js
+        }),
+    ],
+    devServer: {
+        port: 3030, // you can change the port
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".css"],
+        alias: {
+            'inferno': 'inferno/dist/index.dev.esm.js',
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                loader: 'ts-loader',
+                options: {
+                    transpileOnly: true,
+                    experimentalWatchApi: true,
+                }
+            },
+            {
+                test: /\.(png|jpg?g|gif|cur)$/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                type: 'asset/inline'
+            },
+            {
+                test: /\.(eot|ttf|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                type: 'asset/resource'
+            },
+            // {
+            //     test: /\.(le|c)ss$/,
+            //     use: [
+            //         isDevMode ? 'style-loader': MiniCssExtractPlugin.loader,
+            //         'css-loader',
+            //         {
+            //             loader: 'postcss-loader', //shows warnings
+            //             options: {
+            //                 postcssOptions: {
+            //                     plugins: [
+            //                         ["autoprefixer"],
+            //                     ],
+            //                 },
+            //             }
+            //         },
+            //         'less-loader'
+            //     ]
+            // }
+        ],
+    },
+};
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter(),
+  );
+  console.log('qwe', join(__dirname, '../..', 'client', 'dist'),);
+  // const app = await NestFactory.create(AppModule);
+  app.useStaticAssets({
+    root: join(__dirname, '../..', 'client', 'dist'),
+  });
   await app.listen(3002);
 }
 bootstrap();
